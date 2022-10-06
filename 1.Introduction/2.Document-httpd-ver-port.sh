4f9efa883d29eaf888cfbdf650f8ae5edc71a69c
Kavyashree Shah <40597017+kavyashreeshah@users.noreply.github.com>
2022-10-06 16:35:17 +0530

    removed old files

--- 1.Introduction/2.Document-httpd-ver-port.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-httpd -v 2>&1 | awk -F '/' 'NR==1{print $2}' | awk '{print $1}'
-cat /etc/httpd/conf/httpd.conf | grep ^Listen | awk '{print $2}'
--- 6.Operations on Strings/6.Operations on Strings/2.Document-simple-shell-script-to-install-tomcat.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-req_tom_ver="9.0.27"
-tom_m_v=$(echo $req_tom_ver|cut -c 1)
-url="http://mirrors.estointernet.in/apache/tomcat/tomcat-${tom_m_v}/v${req_tom_ver}/bin/apache-tomcat-${req_tom_ver}.tar.gz"
-wget $url
-tar -xvzf apache-tomcat-${req_tom_ver}.tar.gz 
-mv apache-tomcat-${req_tom_ver} tomcat${tom_m_v}
-rm -rf apache-tomcat-${req_tom_ver}.tar.gz 
